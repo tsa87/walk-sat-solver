6f256cebd369edbc619e784c1a46c9bf2e7d504d
Tony <tsa87@sfu.ca>
2020-03-31 13:59:14 -0700

    upload

---
+++ rand_cnf_generator.py
@@ -0,0 +1,10 @@
+
+def rand_cnf_generator(k, m, n):
+"""Generates a Conjunctive Normal Form (CNF) clause.
+    Args:
+      k: # of literals per clause.
+      m: # of clauses to generate.
+      n: # of symbols.
+    Returns:
+      A CNF clause.
+"""
